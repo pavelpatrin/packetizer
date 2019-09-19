3a1817df81eaa236cbbaacd80add601a97f4f794
Pavel Patrin <pavelpatrin@gmail.com>
2019-09-19 20:55:10 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Packetizer',
+    version='1.0',
+    description='PIP to RPM Packetizer',
+    author='Pavel Patrin',
+    author_email='pavelpatrin@gmail.com',
+    url='https://github.com/pavelpatrin/packetizer/',
+    install_requires=['pip', 'requests'],
+)
